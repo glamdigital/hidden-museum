bc5cc59d7ae5316732a5ce51a01d1f6356e0f234
Theodore Koterwas <theodore.koterwas@oucs.ox.ac.uk>
2015-09-22 09:26:38 +0100

    paramaterised sequential instructions

--- app/views/SextantView.js
+++ app/views/SextantView.js
@@ -5,35 +5,39 @@ define(["backbone", "hbs!app/templates/image_capture"],
         template: imageCaptureTemplate,
 
         events: {
-          "click .start-tracking": "startButtonHandler"
+          "click .toggle": "toggleButtonHandler"
         },
 
         serialize: function() {
-
+            var out = {};
+            out.instructions = this.instructions[0];
+            return out;
         },
 
         initialize: function(params) {
             this.isTrackingOrientation = false;
             this.currentDeviceOrientation = {alpha:0, beta:0, gamma:0};
             this.startingDeviceOrientation = {alpha:0, beta:0, gamma:0};
+            this.instructions = ["<ol><li>hold the phone at arm's length</li><li>line up the bottom of the window across the room with the line on the screen</li><li>press the start button</li></ol>", "<p>Now, raise the camera until the fire alarm is aligned with the window sill and press Stop</p>"];
             var tapEnabled = true; //enable tap take picture
             var dragEnabled = false; //enable preview box drag across the screen
             var toBack = true; //send preview box to the back of the webview
             var rect = {x: 0, y: 175, width: 380, height:280};
-            //cordova.plugins.camerapreview.startCamera(rect, "back", tapEnabled, dragEnabled, toBack);
+            cordova.plugins.camerapreview.startCamera(rect, "back", tapEnabled, dragEnabled, toBack);
             $('#content').css("background-color", "transparent");       
         },
-        startButtonHandler: function(ev) {
+        toggleButtonHandler: function(ev) {
             var $target = $(ev.target);
             if (!this.isTrackingOrientation) {
                 //set the starting position to the currentPosition                             
                 $target.text("stop");
-                //this.takeHorizonImage(ev);
+                this.takeHorizonImage(ev);
                 this.startTrackingOrientation(ev);
-                this.displayNextInstructions();
+                this.displayInstructions(1);
             }
             else {
                 this.stopTrackingOrientation(ev);
+                this.displayInstructions(0);
                 $target.text("start");
             }
         },
@@ -70,8 +74,8 @@ define(["backbone", "hbs!app/templates/image_capture"],
             cordova.plugins.camerapreview.takePicture({maxWidth:640, maxHeight:640});
 
         },
-        displayNextInstructions: function() {
-            $("#instructions")[0].innerHTML = "<p>Now, raise the camera until the fire alarm is aligned with the window sill and press Stop</p>";
+        displayInstructions: function(i) {
+            $("#instructions")[0].innerHTML = this.instructions[i];
         }
     });
 
