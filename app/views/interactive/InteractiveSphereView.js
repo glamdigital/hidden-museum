aca9ed7aa793dd5eec08f2e2b3ebba10572fdd19
Andrew Haith <andyhaith@gmail.com>
2015-10-28 15:36:26 +0000

    Drag to rotate sphere

--- app/views/interactive/InteractiveSphereView.js
+++ app/views/interactive/InteractiveSphereView.js
@@ -1,17 +1,25 @@
 define([
         'backbone',
         'three',
+        'hbs!app/templates/interactive/sphere'
     ], function(
         Backbone,
-        three
+        three,
+        sphereTemplate
     )
 {
+    ROTATE_PAN_RATIO = 0.3;     //points of drag to degrees of rotation
+
     var InteractiveSphereView = Backbone.View.extend({
+        template: sphereTemplate,
+
         initialize: function(params) {
             this.texture = params.texture;
         },
+
         afterRender: function () {
-            this.camera = new three.PerspectiveCamera(35, this.$el.width()/this.$el.height(), 0.1, 150);
+            var $container = $('.sphere-container', this.$el);
+            this.camera = new three.PerspectiveCamera(35, $container.width()/$container.height(), 0.1, 150);
             this.camera.position.z = 80;
             thecamera = this.camera;
             this.scene = new three.Scene();
@@ -35,19 +43,43 @@ define([
 
             renderer = new three.WebGLRenderer();
             renderer.setPixelRatio( window.devicePixelRatio );
-            renderer.setSize(this.$el.width(), this.$el.height());
-            this.$el.append(renderer.domElement);
+            renderer.setSize($container.width(), $container.height());
+            $container.append(renderer.domElement);
 
             //window.addEventListener( 'resize', _.bind(this.onWindowResize, this), false );
 
             this.animate();
+
+        },
+
+        events: {
+            //"click .sphere-container": "foo",
+            "touchstart .sphere-container" : "onTouchStart",
+            "touchmove .sphere-container" : "onTouchMove"
+        },
+
+        //foo: function(ev) {
+        //    console.log("clicked on the canvas");
+        //},
+
+        onTouchStart: function(ev) {
+            this.touchPrevX = ev.originalEvent.touches[0].clientX;
+        },
+
+        onTouchMove: function(ev) {
+            var xPos = ev.originalEvent.touches[0].clientX;
+            var deltaX = xPos - this.touchPrevX;
+            console.log('new pos:', xPos, ". delta:", deltaX);
+            this.mesh.rotation.y += deltaX * ROTATE_PAN_RATIO * Math.PI/180;
+
+            this.touchPrevX = xPos;
         },
 
         animate: function() {
             requestAnimationFrame(_.bind(this.animate, this) );
 
             //this.mesh.rotation.x += 0.005;
-            this.mesh.rotation.y += 0.01;
+            //this.mesh.rotation.y += 0.01;
 
             renderer.render(this.scene, this.camera);
         },
