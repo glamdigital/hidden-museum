2a4e26aa758049e9d66edcd2f0d6464b8122d7b9
Andrew Haith <andyhaith@gmail.com>
2015-10-29 10:50:42 +0000

    First pass implementation

--- app/views/TrailView.js
+++ app/views/TrailView.js
@@ -1,5 +1,5 @@
-define(["backbone", "underscore", "app/models/Trail", "app/views/AudioControlsView", "hbs!app/templates/trail"],
-    function(Backbone, _, Trail, AudioControlsView, trail) {
+define(["backbone", "underscore", "app/models/Trail", "hbs!app/templates/trail"],
+    function(Backbone, _, Trail, trail) {
 
     var TrailView = Backbone.View.extend({
 
@@ -9,10 +9,29 @@ define(["backbone", "underscore", "app/models/Trail", "app/views/AudioControlsVi
             this.trails = params.trails;
             this.selectedTrail = params.selectedTrail;
             this.listenTo(Backbone, 'changed_floor', this.changedFloor);
+
+            //store the index for each object within its own gallery
+            this.trails.each( function(trail) {
+               var topics = trail.topics;
+                topics.each( function(topic, index) {
+                   topic.set({galleryIndex: ''+index + topic.attributes.slug});
+                });
+            });
         },
 
         afterRender: function() {
-
+            //position the map markers
+            this.trails.each( function(trail) {
+                var $trailMap = $('#floorplan-' + trail.attributes.slug);
+                //position markers
+                trail.topics.each( function(topic) {
+                    var $marker = $('#map-marker-' + topic.attributes.slug);
+                    var top = $trailMap.height() * topic.attributes.mapY;
+                    $marker.css('top', top + 'px');
+                    var left = $trailMap.width() * topic.attributes.mapX;
+                    $marker.css('left', left + 'px');
+                });
+            });
         },
 
         serialize: function() {
