f9c2af01743844cd64654bef6c9bc353576e1a8b
Giles Burdett <gilesox2015@gmail.com>
2015-11-02 15:05:49 +0000

    Render all blackboards

--- app/views/interactive/BlackboardGalleryView.js
+++ app/views/interactive/BlackboardGalleryView.js
@@ -10,8 +10,33 @@ define([
         var BlackboardGalleryView = Backbone.View.extend({
             template: blackboardGalleryTemplate,
             
+            explanationComponents: [],
+            
+            
+            index: 0,
+            total: 10,
+            
             events: {
+                'click .ui .controls .previous': 'onPrevious',
+                'click .ui .controls .next':     'onNext'
+            },
+            
+            onPrevious: function (event) {
+                // console.log('onPrevious() current index = ' + this.index);
                 
+                if (this.index > 0) {
+                    this.index--;
+                    Backbone.trigger('blackboard_gallery_render');
+                }
+            },
+            
+            onNext: function (event) {
+                // console.log('onNext() current index = ' + this.index);
+                
+                if (this.index < this.total - 1) {
+                    this.index++;
+                    Backbone.trigger('blackboard_gallery_render');
+                }
             },
             
             serialize: function () {
@@ -23,10 +48,83 @@ define([
                 
                 this.overlayInitialize({ displayOnArrival: true });
                 this.overlaySetTemplate(interactiveInnerTemplate, this.model.toJSON());
+                
+                this.listenTo(Backbone, 'blackboard_gallery_render', this.renderGallery);
+            },
+            
+            renderProgress: function () {
+                var jProgress = $('.ui .controls .progress');
+                var jIndex = jProgress.find('.index').first();
+                var jTotal = jProgress.find('.total').first();
+                
+                // We count from zero but users don't so display the index with +1
+                var display = {
+                    index: this.total > 0 ? this.index + 1 : 0,
+                    total: this.total > 0 ? this.total : 0
+                };
+                
+                jIndex.html(display.index);
+                jTotal.html(display.total);
+            },
+            
+            renderExplanation: function () {
+                var jGallery     = $('.ui .gallery');
+                var jBlackboard  = jGallery.find('.blackboard');
+                var jName        = jGallery.find('.name');
+                var jRoles       = jGallery.find('.roles');
+                var jDescription = jGallery.find('.description');
+                
+                var explanation  = this.explanationComponents[this.index];
+                
+                if (explanation.imageRoot) {
+                    jBlackboard.attr('src', 'img/objects/blackboard/gallery/' + explanation.imageRoot + '.jpg');
+                }
+                
+                jName.empty();
+                jName.append(explanation.jAuthor.clone());
+                
+                jRoles.empty();
+                jRoles.append(explanation.jRoles.clone());
+                
+                jDescription.empty();
+                jDescription.append(explanation.jDescription.clone());
+            },
+            
+            renderGallery: function () {
+                this.renderExplanation();
+                this.renderProgress();
             },
             
             afterRender: function () {
+                var self = this;
                 
+                var jExplanations = $('.explanations .explanation');
+                
+                jExplanations.each(function (index, element) {
+                    var i = 0;
+                    var imageRoot = null;
+                    
+                    // Extract a class other than 'explanation', which we assume can be
+                    // used as the corresponding blackboard image name with .jpg
+                    // appended to it in renderExplanation().
+                    for (i = 0; i < this.classList.length; i++) {
+                        if (this.classList[i] !== 'explanation') {
+                            imageRoot = this.classList[i];
+                            break;
+                        }
+                    }
+                    
+                    self.explanationComponents.push({
+                        imageRoot:    imageRoot,
+                        jAuthor:      $(this).find('.author').first().clone(),
+                        jRoles:       $(this).find('.role').clone(),
+                        jDescription: $(this).find('.description').first().clone()
+                    });
+                });
+                
+                this.total = this.explanationComponents.length;
+                
+                this.renderGallery();
             },
             
             cleanup: function () {
