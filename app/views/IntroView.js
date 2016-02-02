611a4ac5911881ac9027bdae5e20531f28f7066b
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2016-02-02 17:11:54 +0000

    create event to hide intro

--- app/views/IntroView.js
+++ app/views/IntroView.js
@@ -7,6 +7,17 @@ define([
 ) {
   var IntroView = Backbone.View.extend({
 		template: introTemplate,
+		
+		hideIntro: function (event) {
+			event.preventDefault();
+			
+		},
+
+		
+		
+		events: {
+				'click #intro-image': 'hideIntro'
+		},
   });
   return IntroView;
 });
