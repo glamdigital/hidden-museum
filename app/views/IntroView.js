419f81e4663f476aa1b694b0b624e312440e2c3e
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2016-02-03 08:57:39 +0000

    auto hide intro after 3secs

--- app/views/IntroView.js
+++ app/views/IntroView.js
@@ -7,13 +7,18 @@ define([
 ) {
   var IntroView = Backbone.View.extend({
 		template: introTemplate,
-		
-		hideIntro: function (event) {
-			event.preventDefault();
-			
+
+		afterRender: function (params) {
+			setTimeout(function(){
+				console.log("timeout");
+				this.hideIntro();
+	    }.bind(this), 3000)
 		},
 
-		
+		hideIntro: function (event) {
+			console.log("go to trails");
+			Backbone.history.navigate('#/trails');
+		},
 		
 		events: {
 				'click #intro-image': 'hideIntro'
